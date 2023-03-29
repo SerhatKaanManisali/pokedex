0dedb5d1f89a0e3c1cfa22cd8bb228e535bc8cd6
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2023-03-29 15:52:04 +0200

    addad bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let currentPokemon;
+
+
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
     let response = await fetch(url);
-    let responseAsJson = await response.json();
+    currentPokemon = await response.json();
+    console.log(currentPokemon);
+
+    renderPokemonInfo();
+}
+
+
+function renderPokemonInfo() {
+    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemon-image').src = currentPokemon['sprites']['other']['home']['front_default']
 }
