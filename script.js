6177c39766d127c4c4cf0beeb07ce1e7826caa14
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2023-03-31 01:40:51 +0200

    added scalable basic pokemon list 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/1`
-    let response = await fetch(url);
-    pokemonList = await response.json();
-    console.log(pokemonList);
-    renderPokemonList();
+let currentPokemon
+
+async function requestPokemon() {
+    for (let i = 1; i < 13; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        renderPokemon(i);  
+
+        console.log(currentPokemon)
+    }
 }
 
 
-function renderPokemonList() {
-    document.getElementById('pokemon-list').innerHTML += pokemonTemplate();
+function renderPokemon(i) {
+    let pokemonImage = currentPokemon['sprites']['other']['home']['front_default'];
+    let rawPokemonName = currentPokemon['name'];
+    let formattedPokemonName = capitalizeFirstLetter(rawPokemonName);
+    pokemonCardTemplate(formattedPokemonName, i);
+    document.getElementById(`pokemon-image${i}`).src = pokemonImage;
+    renderTypeBadge(i);
 }
 
 
-function pokemonTemplate() {
-    return /*html*/`
-        
+function capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+
+function pokemonCardTemplate(formattedPokemonName, i) {
+    return document.getElementById('pokemon-list').innerHTML += /*html*/`
+    <div class="card m-3" style="width: 18rem;">
+        <img id="pokemon-image${i}" src="" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title text-center">${formattedPokemonName}</h5>
+            <span id="type-badge${i}" class="badge bg-gradient"></span>
+        </div>
+    </div>
     `;
+}
+
+
+function renderTypeBadge(i) {
+    let typeName = currentPokemon['types']['0']['type']['name'];
+    let formattedTypeName = capitalizeFirstLetter(typeName);
+    let typeBadge = document.getElementById(`type-badge${i}`);
+
+    typeBadge.classList.add(`${typeName}-badge`);
+    typeBadge.innerHTML = formattedTypeName;
+
+    // TO DO: Show multiple types
 }
